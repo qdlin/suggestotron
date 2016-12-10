d8620a189ddc7cacf5b559f425a8d6c025226671
qdlin <qdlin1314@gmail.com>
2016-12-10 22:13:14 +0800

    add about

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root "topics#index"
   resources :topics do
+    collection do
+     get 'about'
+   end
     member do
       post 'upvote'
       post 'downvote'
     end
   end
+  root "topics#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
